d1a1d50f5a4fdc57cc5f9e2e245d71ef40c43f0d
Valery Palianov <valp@crtweb.ru>
2026-01-07 17:14:36 +0500

    feat: remove App.tsx

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import { app } from './App';
+import express from 'express';
+
+const app = express();
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.get('/', (req, res) => {
+  res.status(404);
+});
 
 const port = 8000;
 
